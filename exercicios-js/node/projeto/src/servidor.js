16ed7d6b03645ac7e229b82bbc1a175345a3318c
Guilherme Caria <guilhermecaria1@gmail.com>
2019-09-29 18:09:42 -0300

    Projeto: API com Express #2

--- exercicios-js/node/projeto/src/servidor.js
+++ exercicios-js/node/projeto/src/servidor.js
@@ -2,15 +2,28 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // Converter para JSON
+    res.send(bancoDeDados.getProdutos) // Converter para JSON
 })
 
 /*app.get('/produtos', (req, res, next) => {
     console.log('Middwleware 1...')
     next()
 })*/
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
 
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}.`)
