c6fb18fa91b2c1a65bb6c0b67ceaa553e018ae56
Guilherme Caria <guilhermecaria1@gmail.com>
2019-10-29 16:17:32 -0300

    Webpack: Desenvolvimento e Produção

--- exercicios-js/webpack/webpack.config.js
+++ exercicios-js/webpack/webpack.config.js
@@ -1,19 +1,31 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: "estilo.css"
         })
     ],
-    module: {
+        module: {
         rules: [{
             test: /\.s?[ac]ss$/,
             use: [
